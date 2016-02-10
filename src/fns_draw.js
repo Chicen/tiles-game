850734c90343f322d8b89a614789f0d5c48ff89c
Nitin <nitin.mdsu@gmail.com>
2016-02-10 20:10:18 +0530

    Init

---
+++ src/fns_draw.js
@@ -0,0 +1,5 @@
+// Final draw stuff resides here
+
+console.log("2");
+
+console.log(a + 100);
---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+// This is a placeholder main.js, really.
+// I'll write code in here until it's fit to move somewhere else
+
+console.log("MAIN");
+
+var a = 1;
