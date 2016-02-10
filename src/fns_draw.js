7d201c0c48bc17342054754e70dcd43555848dc2
Nitin <nitin.mdsu@gmail.com>
2016-02-10 23:18:39 +0530

    Set up play area and padding etc, to start drawing

--- src/fns_draw.js
+++ src/fns_draw.js
@@ -1,5 +1,17 @@
 // Final draw stuff resides here
 
-console.log("2");
 
-console.log(a + 100);
+var draw_bg = function(gs, pa, colors) {
+
+	ctx.fillStyle = colors.sidebar
+	ctx.fillRect(0, 0, gs.le, gs.h)
+
+	ctx.fillStyle = colors.pabg 
+	ctx.fillRect(gs.le, 0, gs.w - gs.le, gs.h)
+
+	ctx.strokeStyle = colors.grid
+	ctx.setLineDash([1, 3])
+	ctx.strokeRect(gs.le + pa.pl, pa.pt, (gs.w - pa.pr) - (gs.le + pa.pl), (gs.h - pa.pb) - (pa.pt))
+
+}
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 
-// This is a placeholder main.js, really.
-// I'll write code in here until it's fit to move somewhere else
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
 
-console.log("MAIN");
+// game screen stuff
+var gs = {
+	w: canvas.width,
+	h: canvas.height,
+	le: canvas.width/3
+}
+// play area stuff
+var pa = {
+	pl: (gs.w - gs.le) / 7, // padding left
+	pr: (gs.w - gs.le) / 7, // padding right
+	pt: (gs.w - gs.le) / 7, // padding top
+	pb: (gs.w - gs.le) / 7, // padding bottom
+	xn: 4,                  // num of tiles in x dir
+	yn: 4,                  // num of tiles in y dir
+	zn: 10                  // num of tiles in z dir
+}
 
-var a = 1;
+var colors = {
+	sidebar: "#00A0B0",
+	pabg:    "#6A4A3C",
+	grid:    "#EDC951"
+}
+
+draw_bg(gs, pa, colors)
